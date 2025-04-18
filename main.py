37bf81168190494c13202d618d85fcd104887a60
Rajshree1102 <146538512+Rajshree1102@users.noreply.github.com>
2025-04-18 17:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def analyze_audio(file: UploadFile = File(...)):
             content={"error": str(e)}
         )
 
-import uvicorn
+# import uvicorn
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8080)
